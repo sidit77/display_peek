cfe15f955476830acfcc7e1bdf828b893437f60d
sidit77 <sidit77@gmail.com>
2023-01-18 01:08:16 +0100

    added functions to draw dxgi textures

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,55 @@
 use raw_window_handle::HasRawWindowHandle;
+use win_desktop_duplication::{co_init, DesktopDuplicationApi, set_process_dpi_awareness};
+use win_desktop_duplication::devices::AdapterFactory;
 use winit::{dpi::*, event::*, event_loop::*, window::*};
+use winit::platform::windows::WindowBuilderExtWindows;
 
 fn main() -> anyhow::Result<()> {
+    set_process_dpi_awareness();
+    co_init();
+
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("mltg d2d")
+        .with_drag_and_drop(false)
         .with_inner_size(LogicalSize::new(1280, 720))
         .build(&event_loop)?;
-    let ctx = mltg::Context::new(mltg::Direct2D::new()?)?;
+    let adapter = AdapterFactory::new().get_adapter_by_idx(0).unwrap();
+    let output = adapter.get_display_by_idx(0).unwrap();
+
+    let mut dupl = DesktopDuplicationApi::new(adapter, output.clone()).unwrap();
+    let (device, _) = dupl.get_device_and_ctx();
+
+    let ctx = mltg::Context::new(mltg::Direct2D::from_d3d(&device)?)?;
     let factory = ctx.create_factory();
     let window_size = window.inner_size();
     let mut render_target = ctx.create_render_target(
         window.raw_window_handle(),
         (window_size.width, window_size.height),
     )?;
-    let brush = factory.create_solid_color_brush((1.0, 1.0, 1.0, 1.0))?;
 
     event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
+        *control_flow = ControlFlow::Poll;
         match event {
             Event::RedrawRequested(_) => {
-                let window_size = window.inner_size().to_logical::<f32>(window.scale_factor());
-                ctx.set_scale_factor(window.scale_factor() as _);
-                ctx.draw(&render_target, |cmd| {
-                    cmd.clear((0.0, 0.0, 0.3, 0.0));
-                    cmd.fill(
-                        &mltg::Rect::from_points((10.0, 10.0), (window_size.width - 10.0, window_size.height - 10.0)),
-                        &brush,
-                    );
-                })
-                    .unwrap();
+                let tex = dupl.acquire_next_frame_now();
+
+                if let Ok(tex) = tex {
+                    let window_size = window.inner_size().to_logical::<f32>(window.scale_factor());
+                    ctx.set_scale_factor(window.scale_factor() as _);
+                    ctx.draw(&render_target, |cmd| {
+                        cmd.clear((0.0, 0.0, 0.3, 0.0));
+                        cmd.draw_bitmap(&factory.create_bitmap(tex.as_raw_ref()).unwrap(),
+                                        mltg::Rect::from_points((10.0, 10.0), (window_size.width - 10.0, window_size.height - 10.0)),
+                                        None,
+                                        mltg::Interpolation::HighQualityCubic);
+                    }).unwrap();
+                }
+
+            },
+            Event::MainEventsCleared => {
+                output.wait_for_vsync().unwrap();
+                window.request_redraw();
             }
             Event::WindowEvent {
                 event: WindowEvent::Resized(size),
